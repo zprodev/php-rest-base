e64c6b2f96691b75f008816bd141a08aeea20ba5
zprodev <zproject.develop@gmail.com>
2018-02-02 20:39:04 +0900

    Change tab to space

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -1,62 +1,62 @@
 <?php
 
 class BaseController {
-	public function receiveRequest() {
-		try {
-			switch ($_SERVER['REQUEST_METHOD']) {
-				case 'POST':
-					$this->post();
-					break;
-				case 'GET':
-					$this->get();
-					break;
-				case 'PUT':
-					$this->put();
-					break;
-				case 'DELETE':
-					$this->delete();
-					break;
-				default:
-					break;
-			}
-		} catch (Exception $e) {
-			$this->failure(['message' => DEBUG ? $e->getMessage() : 'Internal Server Error']);
-		}
-		$this->failure(null, 405);
-	}
+    public function receiveRequest() {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'POST':
+                    $this->post();
+                    break;
+                case 'GET':
+                    $this->get();
+                    break;
+                case 'PUT':
+                    $this->put();
+                    break;
+                case 'DELETE':
+                    $this->delete();
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception $e) {
+            $this->failure(['message' => DEBUG ? $e->getMessage() : 'Internal Server Error']);
+        }
+        $this->failure(null, 405);
+    }
 
-	protected function getPDO() {
-		static $pdo;
-	
-		if (!isset($pdo)) {
-			$dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8';
-			$pdo = new PDO( $dsn, DB_USER, DB_PASS);
-			$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-		}
-		return $pdo;
-	}
-	
-	protected function post(){}
-	protected function get(){}
-	protected function put(){}
-	protected function delete(){}
+    protected function getPDO() {
+        static $pdo;
+    
+        if (!isset($pdo)) {
+            $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8';
+            $pdo = new PDO( $dsn, DB_USER, DB_PASS);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        return $pdo;
+    }
+    
+    protected function post(){}
+    protected function get(){}
+    protected function put(){}
+    protected function delete(){}
 
-	protected function success($data = null){
-		header("Content-Type: application/json; charset=utf-8", true, 200);
-		if($data){
-			echo json_encode($data);
-		}
-		exit;
-	}
+    protected function success($data = null){
+        header("Content-Type: application/json; charset=utf-8", true, 200);
+        if($data){
+            echo json_encode($data);
+        }
+        exit;
+    }
 
-	protected function failure($data = null, $code = 500){
-		header("Content-Type: application/json; charset=utf-8", true, $code);
-		if($data){
-			echo json_encode($data);
-		}
-		exit;
-	}
+    protected function failure($data = null, $code = 500){
+        header("Content-Type: application/json; charset=utf-8", true, $code);
+        if($data){
+            echo json_encode($data);
+        }
+        exit;
+    }
 }
 
--- router.php
+++ router.php
@@ -2,8 +2,8 @@
 
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 if(!$uri){
-	http_response_code('400');
-	exit;
+    http_response_code('400');
+    exit;
 }
 $uris = explode('/', $uri);
 $resource = $uris[0];
@@ -11,8 +11,8 @@ $resource = $uris[0];
 $className = ucfirst($resource.'Controller');
 $classFilePath = dirname(__FILE__) . '/controller/' . $className . '.php';
 if(!file_exists($classFilePath)){
-	http_response_code('404');
-	exit;
+    http_response_code('404');
+    exit;
 }
 
 require_once( dirname(__FILE__).'/conf/const.php' );
